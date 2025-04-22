b288a49b0bc5e160c7008b68ec39d664ff9e6b5e
Youssef <yousef.ahmed.hani@gmail.com>
2025-04-22 20:00:46 +0200

    split it into different files and added a basic scoring system

---
+++ main.js
@@ -0,0 +1,146 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+const rows = 6;
+const cols = 6;
+const cardSize = 100;
+var chosenCards = false;
+var card1;
+var card2;
+var busy = false;
+
+const cards = [];
+const imgs = new Array(18).fill(0);
+
+// Variables for scoring system
+let start_score = 1000;
+let score = start_score;
+
+for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+        cards.push({
+            x: col * cardSize,
+            y: row * cardSize,
+            visible: true,
+            image: null,
+            imgid: null,
+        });
+    }
+}
+
+drawImages();
+
+function drawBoard() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (const card of cards) {
+        if (card.visible) {
+            drawCard(card.x, card.y);
+        } else {
+            if (card.image && card.image.complete) {
+                ctx.drawImage(
+                    card.image,
+                    card.x,
+                    card.y,
+                    cardSize - 2,
+                    cardSize - 2
+                );
+            }
+        }
+    }
+
+    let scoreDiv = document.getElementById("score");
+    scoreDiv.innerHTML = score;
+}
+
+function drawCard(x, y) {
+    ctx.fillStyle = "#cccccc";
+    ctx.fillRect(x, y, cardSize - 2, cardSize - 2);
+    ctx.strokeStyle = "#999";
+    ctx.strokeRect(x, y, cardSize - 2, cardSize - 2);
+}
+
+function pickrand() {
+    do {
+        var i = Math.floor(Math.random() * 18);
+    } while (imgs[i] == 2);
+
+    imgs[i]++;
+    console.log(i);
+    return i;
+}
+
+function drawImages() {
+    for (let idx = 0; idx < rows * cols; idx++) {
+        const rnd = pickrand();
+        const imgPath = `images/${rnd + 1}.jpg`;
+
+        const image = new Image();
+        image.src = imgPath;
+
+        cards[idx].image = image;
+        cards[idx].imgid = rnd;
+    }
+}
+
+function flipCard(card) {
+    if (busy || card == card1) return;
+    if (chosenCards == false) {
+        card1 = card;
+        card1.visible = false;
+        drawBoard();
+        chosenCards = true;
+        console.log("revealing first card");
+    } else {
+        console.log("revealing second card for 1 sec");
+        card2 = card;
+        card2.visible = false;
+        drawBoard();
+        busy = true;
+        setTimeout(() => {
+            if (card1.imgid != card2.imgid) {
+                card1.visible = true;
+                card2.visible = true;
+                updateScore(50);
+            } else {
+                // will write a function here
+            }
+            console.log("redrawing");
+            drawBoard();
+            chosenCards = false;
+            card1 = null;
+            card2 = null;
+            busy = false;
+        }, 1500);
+    }
+}
+
+canvas.addEventListener("click", (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const clickX = event.clientX - rect.left;
+    const clickY = event.clientY - rect.top;
+    var card1;
+    var card2;
+    for (const card of cards) {
+        if (
+            card.visible &&
+            clickX >= card.x &&
+            clickX < card.x + cardSize &&
+            clickY >= card.y &&
+            clickY < card.y + cardSize
+        ) {
+            flipCard(card);
+
+            break;
+        }
+    }
+});
+
+function updateScore(penalty) {
+    score = score - penalty;
+    if (score <= 0) {
+        alert("you failed");
+        location.reload();
+    }
+}
+
+drawBoard();
